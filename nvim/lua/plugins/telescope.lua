4a065b6020444029b2a28243909d8fc445425ffb
Matvei Kotenev <kotenev.matvei@gmail.com>
2024-11-17 11:44:36 +0100

    added first LSPs!

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,17 +1,34 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>ff', 
-      function()
-        builtin.find_files({
-          prompt_title = "Find Files",
-          hidden = true  -- This includes hidden files and directories
-        })
-      end
-    )
-  end
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>ff',
+        function()
+          builtin.find_files({
+            prompt_title = "Find Files",
+            hidden = true  -- This includes hidden files and directories
+          })
+        end
+      )
+    end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+              -- even more opts
+            }
+          }
+        }
+      }
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
