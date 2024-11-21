d992b0d0d0b6956b20a3980fe6f5bade89601030
Matvei Kotenev <kotenev.matvei@gmail.com>
2024-11-21 22:11:17 +0100

    added completions

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -3,26 +3,62 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "pyright", "clangd"}
+        ensure_installed = { "lua_ls", "pyright", "clangd", "ruff", "jedi_language_server" },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.clangd.setup{}
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+      lspconfig.lua_ls.setup({capabilities = capabilities})
+      lspconfig.pyright.setup({
+        settings = {
+          -- pyright = {
+          --   disableOrganizeImports = true,
+          -- },
+          python = {
+            analysis = {
+              ignore = { "*" },
+            },
+          },
+        },
+        capabilities = capabilities,
+      })
+      lspconfig.jedi_language_server.setup({
+        on_attach = function(client)
+          client.server_capabilities.hoverProvider = true
+        end,
+        capabilities = capabilities,
+      })
+      lspconfig.clangd.setup({capabilities = capabilities})
+      lspconfig.ruff.setup({capabilities = capabilities})
+
+      -- Disable hover in favor of Pyright
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client == nil then
+            return
+          end
+          if client.name == "ruff" then
+            client.server_capabilities.hoverProvider = false
+          end
+        end,
+        desc = "LSP: Disable hover capability from Ruff",
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>do", vim.lsp.buf.document_symbol, {})
+    end,
+  },
 }
---
+++ nvim/lua/test.py
@@ -0,0 +1,5 @@
+import logging
+print()
+def hello_world():
+
+    pass
