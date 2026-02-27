3a12ab22133c8c1acbbc2ee47480503f41d0d429
Matvei Kotenev <kotenev.matvei@gmail.com>
2026-02-27 15:22:01 +0100

    add ideavimrc, ipython

---
+++ ipython/ipython_config.py
@@ -0,0 +1,4 @@
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
+c.TerminalInteractiveShell.cursor_shape_changes_in_vi = True
+c.TerminalInteractiveShell.auto_match = True
